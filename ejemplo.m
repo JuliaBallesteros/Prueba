43bb64185e444f1121388447e1396664d56ae49a
JuliaBallesteros <julia.ballesteros@bitsphi.com>
2024-04-10 13:27:59 +0200

    cambio desde branch1

--- ejemplo.m
+++ ejemplo.m
@@ -1,12 +1,6 @@
 
-disp('Este es un script de prueba para sourcetree y github')
-disp('prueba3')
 
-%% cambio
-disp('cambio para commit desde sourcetree')
 
-%
-% prueba cambiio desde main que no llevarme a mi branch para generar conflict 
+%% PRUEBA cambio desde BRANCH1 que quiero llevar a main 
 
-%% PRUEBA SWITCH BRANCH SIN COMITEAR CAMBIOS
 
