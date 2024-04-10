4f16006869d1f18bbfb4e0fc0e2666b5a30509c2
JuliaBallesteros <julia.ballesteros@bitsphi.com>
2024-04-10 14:47:00 +0200

    scripts nuevos para pruebas
    scripts nuevos desde pruebas (subidos desde main)

---
+++ script_2.m
@@ -0,0 +1,8 @@
+% ejemplo de script en el que no se haran casi cambios y si se hacen si 
+% que querremos traerlos desde main a nuestro branch (si el cambio no le
+% hemos hecho nosotros) o desde nuestro branch a main (si el cambio le
+% hemos hecho nosotros) 
+
+a = 1;
+b = 3;
+c = a + b
---
+++ script_rutas.m
@@ -0,0 +1,4 @@
+% script de prueba tipo setVariables: con rutas que cambian para cada uno 
+% (haremos cambios que no queremos llevar desde nuestro branch a main) 
+
+disp('ruta_sujetos')
