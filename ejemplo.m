eda568fc61dcbec2599c3ca191b41857ff8635ec
gerardogalvezbitsphi <gerardo.galvez@bitsphi.com>
2024-04-23 17:33:09 +0200

    Update ejemplo.m

--- ejemplo.m
+++ ejemplo.m
@@ -1,7 +1,7 @@
 Cambio Roldán
 yo gerardo hago cambio como si fuera roldan
-yo julia cambio 
-yo roldan otro cambio 1712R
+yo julia cambio 18 
+yo roldan otro cambiar
 %% PRUEBA cambio desde BRANCH1 que quiero llevar a main 
 
 
