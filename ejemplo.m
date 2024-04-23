09af8cb960ce40747e80a63875058c7367843c6f
RoldanMartinez <roldan.martinez@bitsphi.com>
2024-04-23 17:20:34 +0200

    Update ejemplo.m

--- ejemplo.m
+++ ejemplo.m
@@ -1,7 +1,7 @@
 Cambio Roldán
 yo gerardo hago cambio como si fuera roldan
 yo julia cambio 
-yo roldan otro cambio 16 52
+yo roldan otro cambio 1712R
 %% PRUEBA cambio desde BRANCH1 que quiero llevar a main 
 
 
