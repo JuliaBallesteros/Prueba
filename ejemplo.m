d7db5deba787ab221ee262aa8059c2150a7c64cf
JuliaBallesteros <julia.ballesteros@bitsphi.com>
2024-04-23 17:20:29 +0200

    update ejemplo julia
    update desde branch1_julia para generar conflicto

--- ejemplo.m
+++ ejemplo.m
@@ -1,7 +1,7 @@
 Cambio Roldán
 yo gerardo hago cambio como si fuera roldan
 yo julia cambio 
-yo roldan otro cambio 16 52
+yo roldan otro cambio 18 
 %% PRUEBA cambio desde BRANCH1 que quiero llevar a main 
 
 
